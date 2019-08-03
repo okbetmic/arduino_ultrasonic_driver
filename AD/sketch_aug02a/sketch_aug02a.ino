a9e80dcee7629fc56fd5162ac9136d0ef2c2aa39
super1klim <super1klim@gmail.com>
2019-08-03 14:16:34 +0300

    Update sketch_aug02a.ino

--- AD/sketch_aug02a/sketch_aug02a.ino
+++ AD/sketch_aug02a/sketch_aug02a.ino
@@ -1,7 +1,7 @@
-#include <Wire.h> 
+#include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 #include "GyverEncoder.h"
-#include <AD9850.h> 
+#include <AD9850.h>
 
 //LCD pins
 #define I2C_ADR 0x27
@@ -11,7 +11,7 @@
 //DDS AD pins
 #define AD_W_CLK 6
 #define AD_FQ_UD 7
-#define AD_DATA_D7 8 
+#define AD_DATA_D7 8
 #define RESET 9
 
 //Encoder pins
@@ -19,73 +19,81 @@
 #define EN_DT 3
 #define EN_SW 4
 
-#define MAX_STEP 1000000
+#define MAX_STEP 100000
 #define MIN_STEP 1
 
-#define phase 0
-#define freq_min 1
-#define freq_max 9999999
+#define F_DELAY 0
+
+#define PHASE 0
+#define MIN_FREQ 1
+#define MAX_FREQ 1000000
 
 Encoder E(EN_CLK, EN_DT, EN_SW);
-LiquidCrystal_I2C lcd(I2C_ADR, symbolscount, stringscount); //i2c-адрес, кол-во символов, кол-во строк
+LiquidCrystal_I2C lcd(I2C_ADR, symbolscount, stringscount);
 
-int freq_step = 1;
-int freq = 1;
+long freq_step = 1;
+long freq = 1;
 
 
 void setup() {
   DDS.begin(AD_W_CLK, AD_FQ_UD, AD_DATA_D7, RESET);
-  
+
+  E.setType(TYPE1);
+
   lcd.begin();
   lcd.clear();
-  
+
   lcd.setCursor(0, 0);
   lcd.print("freq: ");
   lcd.setCursor(6, 0);
   lcd.print(freq);
-  
+
   lcd.setCursor(0, 1);
   lcd.print("step: ");
   lcd.setCursor(6, 1);
   lcd.print(freq_step);
-  
-  DDS.setfreq(freq, phase);
+
+  DDS.setfreq(freq, PHASE);
 }
 
 void loop() {
   E.tick();
-  if(E.isRight() && freq + freq_step <= freq_max){
-    freq+= freq_step;
+  if (E.isRight() && freq + freq_step <= MAX_FREQ) {
+    freq += freq_step;
     freq_change();
+    delay(F_DELAY);
   }
-  
-  if(E.isLeft() && freq - freq_step >= freq_min){
-    freq-= freq_step;
+
+  if (E.isLeft() && freq - freq_step >= MIN_FREQ) {
+    freq -= freq_step;
     freq_change();
+    delay(F_DELAY);
   }
-  
-  if(E.isRightH()){
+
+  if (E.isRightH()) {
     if (freq_step < MAX_STEP)
     {
       freq_step *= 10;
       step_change();
+      delay(F_DELAY);
     }
   }
-  if(E.isLeftH()){
+  if (E.isLeftH()) {
     if (freq_step > MIN_STEP)
     {
       freq_step /= 10;
       step_change();
+      delay(F_DELAY);
     }
   }
 }
 
-void freq_change(){
-  DDS.setfreq(freq, phase);
+void freq_change() {
+  DDS.setfreq(freq, PHASE);
 
   lcd.setCursor(6, 0);
   lcd.print("          ");
-   lcd.setCursor(6, 0);
+  lcd.setCursor(6, 0);
   lcd.print(freq);
 }
 
