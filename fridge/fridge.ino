81b3f063bde47862cf69ffab3c5ccde07e47275b
valyankilyan <valentin.kil.2002@gmail.com>
2019-09-07 14:28:36 +0300

    add fridge logger

--- fridge/fridge.ino
+++ fridge/fridge.ino
@@ -1,5 +1,7 @@
 #include <OneWire.h>
 #include <DallasTemperature.h>
+#include <SPI.h>
+#include <SD.h>
 
 #define ONE_WIRE_BUS 2
 #define DELAY 30*1000
@@ -11,8 +13,12 @@ int deviceCount = 0;
 float tempC;
 unsigned long t = 0;
 
+const int chipSelect = 10;
+
 void setup(void)
 {
+  SD.begin(chipSelect);
+  
   sensors.begin();
   Serial.begin(9600);
   Serial.print("Locating devices...");
@@ -26,21 +32,23 @@ void setup(void)
 
 }
 
-void loop(void)
-{
-  if ((unsigned long)(millis() - t) >= DELAY)
-  {
+void loop(void){
+  if ((unsigned long)(millis() - t) >= DELAY){
+    File dataFile = SD.open("fridgelog.txt", FILE_WRITE);
+    
     t = millis();
     Serial.print(t);
     Serial.print("\t");
 
     sensors.requestTemperatures();
-    for (int i = 0;  i < deviceCount;  i++)
-    {
+    for (int i = 0;  i < deviceCount;  i++){
       tempC = sensors.getTempCByIndex(i);
       Serial.print(tempC);
       Serial.print("\t");
+      dataFile.print(tempC);
+      dataFile.print('\t');
     }
+    dataFile.print('\n');
     Serial.println("");
 
   }
